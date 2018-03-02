5620d092a590274a23071383a798dec08bac86aa
Lin Wang <lin.wang@healthcatalyst.com>
2018-03-01 21:11:38 -0700

    all done

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -4,7 +4,12 @@ let id = 0;
 module.exports = {
   create: ( req, res ) => {
     const { text, time } = req.body;
+
+    
+
     messages.push({ id, text, time });
+    req.session.user.messages.push({id, text, time});
+
     id++;
     res.status(200).send( messages );
   },
@@ -15,7 +20,7 @@ module.exports = {
 
   update: ( req, res ) => {
     const { text } = req.body;
-    const updateID = req.params.id;
+    const updateID = req.query.id;
     const messageIndex = messages.findIndex( message => message.id == updateID );
     let message = messages[ messageIndex ];
 
@@ -29,9 +34,13 @@ module.exports = {
   },
 
   delete: ( req, res ) => {
-    const deleteID = req.params.id;
+    const deleteID = req.query.id;
     messageIndex = messages.findIndex( message => message.id == deleteID );
     messages.splice(messageIndex, 1);
     res.status(200).send( messages );
+  },
+
+  history: (req,res)=> {
+    res.status(200).send(req.session.user.messages)
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,37 @@
 const express = require('express');
+const session=require('express-session');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+require('dotenv').config()
+
+ const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter.js');
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+   secret:process.env.SESSION_SECRET,
+   resave: false,
+   saveUninitialized: true,
+   cookie:
+    {maxAge:10000}
+}));
+
+ app.use((req,res,next)=>createInitialSession(req,res,next));
+app.use((req,res,next)=>{
+    if(req.method==='POST' || req.method==='PUT'){filter(req,res,next);}
+    else {next();}
+});
+
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
+app.get(messagesBaseUrl+'/history', mc.history);
 
 const port = process.env.PORT || 3000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
